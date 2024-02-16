eb74e0fe9e772cd55237cb98d5dcdd808a19a855
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-02-16 21:59:25 +0800

    in line 25 add sys.exit

--- src/Hongxue_jieba.py
+++ src/Hongxue_jieba.py
@@ -1,3 +1,4 @@
+import sys                      # 系統控制模組
 import os.path                  # 系統功能模組
 import numpy                    # 分析模組
 
@@ -19,10 +20,9 @@ from jiayan import CharHMMTokenizer
 # 匯入 data
 with open('Dream_of_the_Red_Chamber.txt', 'r', encoding='utf-8') as f:
     data = f.read()
-    # print(data)
+    print(type(data))  # 字串，非list
     # print('------------------')
-
-
+# sys.exit(0)  # 正常離開程式
 
 # 定義繁體中文檔名
 WORDS_PATH = 'dict.txt.big.txt' # 繁體中文詞庫檔名
