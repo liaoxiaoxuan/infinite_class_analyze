d599fe487a5ea611ef069d47b27978631940da3a
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-02-18 23:36:47 +0800

    revice input data and add split

--- src/Hongxue_word2vec.py
+++ src/Hongxue_word2vec.py
@@ -23,71 +23,71 @@ jieba.load_userdict("Hongxue_place.txt")  # 地名
 
 
 
-# 匯入 data
-with open('Dream_of_the_Red_Chamber.txt', 'r', encoding='utf-8') as f:
-    data = f.read()
-    # print(type(data))  # 字串，非list
-    # print('------------------')
+# 匯入 data，並用「取代」進行分句
+f = open('Dream_of_the_Red_Chamber.txt', encoding='utf-8')
+f = f.read().split("。")
+# print(list(f))
+
 # sys.exit(0)  # 正常離開程式
 
-# 定義繁體中文檔名
-WORDS_PATH = 'dict.txt.big.txt' # 繁體中文詞庫檔名
-TC_FONT_PATH = './NotoSerifTC-Regular.otf' # 繁體中文字型檔名
+# # 定義繁體中文檔名
+# WORDS_PATH = 'dict.txt.big.txt' # 繁體中文詞庫檔名
+# TC_FONT_PATH = './NotoSerifTC-Regular.otf' # 繁體中文字型檔名
 
-# 匯入圖表中文字體
-font_path = "./NotoSerifTC-Regular.otf"  # 替換為實際的中文字體文件路徑
-font_prop = FontProperties(fname=font_path)
+# # 匯入圖表中文字體
+# font_path = "./NotoSerifTC-Regular.otf"  # 替換為實際的中文字體文件路徑
+# font_prop = FontProperties(fname=font_path)
 
-# 設定中文字體
-font_path = r"D:/PYTHON/infinite_class_analyze/src/NotoSerifSC-Regular.otf"  # 替換為實際的中文字體文件路徑
-font_prop = FontProperties(fname=font_path)
+# # 設定中文字體
+# font_path = r"D:/PYTHON/infinite_class_analyze/src/NotoSerifSC-Regular.otf"  # 替換為實際的中文字體文件路徑
+# font_prop = FontProperties(fname=font_path)
 
 
 
-# jieba 斷詞
+# # jieba 斷詞
 
-# # 精確模式
-# for sentence in data:
-#     seg_list = jieba.lcut(data)
-#     print('/'.join(seg_list))
+# # # 精確模式
+# # for sentence in data:
+# #     seg_list = jieba.lcut(data)
+# #     print('/'.join(seg_list))
     
-#     print('---------------')
+# #     print('---------------')
 
-# # 全模式
-# for sentence in data:
-#     seg_list = jieba.cut(sentence, cut_all=True)
-#     print('/'.join(seg_list))
+# # # 全模式
+# # for sentence in data:
+# #     seg_list = jieba.cut(sentence, cut_all=True)
+# #     print('/'.join(seg_list))
 
-# print('---------------')
+# # print('---------------')
 
-# # 搜索引擎模式
-# for sentence in data:
-#     seg_list = jieba.cut_for_search(sentence)
-#     print('/'.join(seg_list))
+# # # 搜索引擎模式
+# # for sentence in data:
+# #     seg_list = jieba.cut_for_search(sentence)
+# #     print('/'.join(seg_list))
 
-# jieba.lcut () 模式
-seg_list = jieba.lcut(data)
-words = []
-# print(seg_list)
+# # jieba.lcut () 模式
+# seg_list = jieba.lcut(data)
+# words = []
+# # print(seg_list)
 
 
 
-# 過濾掉所有的標點符號
+# # 過濾掉所有的標點符號
 
-for i in seg_list:
-    # 將 seg_list 中的每個元素 i 中的特定字符（例如標點符號和特殊字元）替換為空字串
-    # i = re.sub("尋找字符", "取代字符", i)
-    i = re.sub("[\s+\.\!\/_,$%^*(+\"\'””《》「」〔〕『』；［］■●...〈〉【】]+|[+——！，。？、~@#￥%……&*（）：]+", "", i)
-    if len(i) > 0:
-        words.append(i)
-# print(len(words))
-# print(words)
+# for i in seg_list:
+#     # 將 seg_list 中的每個元素 i 中的特定字符（例如標點符號和特殊字元）替換為空字串
+#     # i = re.sub("尋找字符", "取代字符", i)
+#     i = re.sub("[\s+\.\!\/_,$%^*(+\"\'””《》「」〔〕『』；［］■●...〈〉【】]+|[+——！，。？、~@#￥%……&*（）：]+", "", i)
+#     if len(i) > 0:
+#         words.append(i)
+# # print(len(words))
+# # print(words)
 
-# # 建立 trigrams 的列表
-# # Trigrams 是一種 NLP 中常用的技術，用於建立文本的模型，特別是在語言生成或預測下一個詞語時
-# # 建立一個包含文本中所有可能的 trigram 的列表，輸出前三個 trigrams，以供檢視
-# trigrams = [([words[i], words[i + 1]], words[i + 2]) for i in range(len(words) - 2)]
-# print(trigrams[:3])
+# # # 建立 trigrams 的列表
+# # # Trigrams 是一種 NLP 中常用的技術，用於建立文本的模型，特別是在語言生成或預測下一個詞語時
+# # # 建立一個包含文本中所有可能的 trigram 的列表，輸出前三個 trigrams，以供檢視
+# # trigrams = [([words[i], words[i + 1]], words[i + 2]) for i in range(len(words) - 2)]
+# # print(trigrams[:3])
 
 
 
@@ -98,5 +98,5 @@ for i in seg_list:
 #     file.write(" ".join(counter))
 
 # 結果是dict
-with open("Hongxue_word2vec_lcut.txt", "w", encoding="utf-8") as file:
-    file.write(str(words))
+# with open("Hongxue_word2vec_split.txt", "w", encoding="utf-8") as file:
+#     file.write(str(list(f)))
